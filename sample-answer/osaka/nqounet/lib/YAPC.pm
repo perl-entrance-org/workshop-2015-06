af603f5ab508bdb7a73cef344339b78abd736d85
nqounet <mail@nqou.net>
2016-03-17 10:11:47 +0900

    ワンランク上の実装でリファクタリング(master)

--- sample-answer/osaka/nqounet/lib/YAPC.pm
+++ sample-answer/osaka/nqounet/lib/YAPC.pm
@@ -15,37 +15,17 @@ sub day {20}
 sub is_yet {
     my $date = shift;
 
-    my ($year, $month, $day) = split '/', $date;
+    my $target_time = Time::Piece->strptime($date, '%Y/%m/%d');
 
-    # 年を確認
-    if ($year > year()) {
-        return;
-    }
-    if ($year < year()) {
-        return 1;
-    }
+    my $yapc_date = sprintf '%04d/%02d/%02d', year(), month(), day();
+    my $yapc_time = Time::Piece->strptime($yapc_date, '%Y/%m/%d');
 
-    # 月を確認
-    if ($month > month()) {
-        return;
-    }
-    if ($month < month()) {
-        return 1;
-    }
-
-    # 日を確認
-    if ($day < day()) {
-        return 1;
-    }
-    return;
+    return $target_time < $yapc_time;
 }
 
 sub is_yet_now {
     my $now = localtime;
-
-    my $yapc_date = sprintf '%04d/%02d/%02d', year(), month(), day();
-    my $yapc_time = Time::Piece->strptime($yapc_date, '%Y/%m/%d');
-    return $now < $yapc_time;
+    is_yet($now->strftime('%Y/%m/%d'));
 }
 
 1;
